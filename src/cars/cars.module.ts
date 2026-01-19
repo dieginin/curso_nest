2991cfe753c31e65c706bc5efbad00211c2cfd5e
dieginin <diegobalestra9@gmail.com>
2026-01-18 18:29:26 -0800

    Implement CarsService for better data management and refactor CarsController to utilize the service

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,7 +1,9 @@
 import { CarsController } from './cars.controller';
+import { CarsService } from './cars.service';
 import { Module } from '@nestjs/common';
 
 @Module({
   controllers: [CarsController],
+  providers: [CarsService],
 })
 export class CarsModule {}
