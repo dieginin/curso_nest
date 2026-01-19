0f01b703617cc3ba8f3e3fe0da506725f83c8f65
dieginin <diegobalestra9@gmail.com>
2026-01-19 07:59:16 -0800

    Add CreateCarDto for structured car creation data in CarsController

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dtos/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -26,8 +27,8 @@ export class CarsController {
   }
 
   @Post()
-  createCar(@Body() body: object) {
-    return body;
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return createCarDto;
   }
 
   @Patch(':id')
