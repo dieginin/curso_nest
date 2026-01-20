63809a34871403d1a986e8ade0453b2f3e4226ac
dieginin <diegobalestra9@gmail.com>
2026-01-19 21:09:14 -0800

    Add Seed module with controller and service; update AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { BrandsModule } from './brands/brands.module';
 import { CarsModule } from './cars/cars.module';
 import { Module } from '@nestjs/common';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [CarsModule, BrandsModule],
+  imports: [CarsModule, BrandsModule, SeedModule],
   controllers: [],
   providers: [],
   exports: [],
