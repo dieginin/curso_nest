c593a8fff66ceaaa84211de92d8bf63fb3b10198
dieginin <diegobalestra9@gmail.com>
2026-01-20 21:39:26 -0800

    Add SeedModule, SeedService, and PokeResponse interface for Pokémon data seeding

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,8 +1,81 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke.response';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return `Seed executed`;
+  constructor(private readonly httpService: HttpService) {}
+
+  async executeSeed() {
+    const {
+      data: { results },
+    } = await firstValueFrom(
+      this.httpService.get<PokeResponse>('pokemon?limit=650'),
+    );
+
+    results.forEach(({ name, url }) => {
+      const no = +url.split('/')[-2];
+      console.log({ name, no });
+    });
+    return results;
   }
 }
+
+// import {
+//   Injectable,
+//   InternalServerErrorException,
+//   Logger,
+// } from '@nestjs/common';
+// import { catchError, firstValueFrom } from 'rxjs';
+
+// import { AxiosError } from 'axios';
+// import { HttpService } from '@nestjs/axios';
+// import { PokeResponse } from './interfaces/poke.response';
+
+// @Injectable()
+// export class SeedService {
+//   private readonly logger = new Logger(SeedService.name);
+//   constructor(private readonly httpService: HttpService) {}
+
+//   async executeSeed() {
+//     const {
+//       data: { results },
+//     } = await firstValueFrom(
+//       this.httpService.get<PokeResponse>('pokemon?limit=650').pipe(
+//         catchError((error: AxiosError) => {
+//           this.logger.error(error.message);
+//           throw new InternalServerErrorException('Error fetching pokemons');
+//         }),
+//       ),
+//     );
+
+//     results.forEach(({ name, url }) => {
+//       const no = Number(url.split('/').at(-2));
+//       console.log({ name, no });
+//     });
+//     return results;
+//   }
+// }
+
+// import axios, { AxiosInstance } from 'axios';
+
+// import { Injectable } from '@nestjs/common';
+// import { PokeResponse } from './interfaces/poke.response';
+
+// @Injectable()
+// export class SeedService {
+//   private readonly axios: AxiosInstance = axios;
+
+//   async executeSeed() {
+//     const { data } = await this.axios.get<PokeResponse>(
+//       'https://pokeapi.co/api/v2/pokemon?limit=650',
+//     );
+//     data.results.forEach(({ name, url }) => {
+//       const segments = url.split('/');
+//       const no = +segments[segments.length - 2];
+//       return { name, no };
+//     });
+//     return data.results;
+//   }
+// }
