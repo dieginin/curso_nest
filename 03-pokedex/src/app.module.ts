e9cdbd9f9a07003ddc220d16b2579e24fd627341
dieginin <diegobalestra9@gmail.com>
2026-01-21 22:49:52 -0800

    Add Joi validation schema and update ConfigModule for environment variables

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,6 +1,7 @@
 import { AppConfig } from './config/app.config';
 import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
+import { JoiValidationSchema } from './config/joi.validation';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -10,7 +11,10 @@ import { join } from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ load: [AppConfig] }),
+    ConfigModule.forRoot({
+      load: [AppConfig],
+      validationSchema: JoiValidationSchema,
+    }),
     MongooseModule.forRoot(process.env.MONGODB!),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     PokemonModule,
--- 03-pokedex/src/config/app.config.ts
+++ 03-pokedex/src/config/app.config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = () => ({
   environment: process.env.NODE_ENV || 'dev',
   mongoDb: process.env.MONGODB!,
-  port: process.env.PORT || 3000,
-  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+  port: process.env.PORT,
+  defaultLimit: Number(process.env.DEFAULT_LIMIT),
 });
