12d2d1ef9b74d80a323fe40726bd6398bc0568e2
dieginin <diegobalestra9@gmail.com>
2026-01-23 21:00:22 -0800

    Enhance ProductsService update method with error handling and change ID type to string

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -50,8 +50,20 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    try {
+      const product = await this.productRepository.preload({
+        id,
+        ...updateProductDto,
+      });
+      if (!product)
+        throw new NotFoundException(`Product with id ${id} not found`);
+
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBError(error as DatabaseError);
+    }
   }
 
   async remove(id: string) {
