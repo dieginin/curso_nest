0bf971709a0c3ff18b21b195335ce4f9610d5ed1
dieginin <diegobalestra9@gmail.com>
2026-01-25 15:46:02 -0800

    Add image upload validation and retrieval functionality in FilesController and FilesService

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,8 +1,12 @@
 import {
+  BadRequestException,
   Controller,
   FileTypeValidator,
+  Get,
+  Param,
   ParseFilePipe,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -11,6 +15,7 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileNamer } from './helpers/fileNamer.helper';
+import type { Response } from 'express';
 
 const ImageValidationPipe = new ParseFilePipe({
   fileIsRequired: true,
@@ -39,6 +44,18 @@ export class FilesController {
     @UploadedFile(ImageValidationPipe)
     file: Express.Multer.File,
   ) {
-    return { fileName: file.originalname };
+    if (!file) throw new BadRequestException('A file must be attached');
+
+    const secureUrl = `${file.filename}`;
+    return { secureUrl };
+  }
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
   }
 }
