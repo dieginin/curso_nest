6d33202a88dcbd7c8213257d6964f047cfbdb6cc
dieginin <diegobalestra9@gmail.com>
2026-01-27 23:03:58 -0800

    Add JWT support and update environment configuration for authentication

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -1,13 +1,22 @@
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: '2hr' },
+    // }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
