fc110b747bb43aec1a4b851867211f747105fa88
dieginin <diegobalestra9@gmail.com>
2026-01-28 16:07:29 -0800

    Add GetUser decorator to retrieve user from request in private route

---
+++ 04-teslo-shop/src/auth/decorators/get-user.decorator.ts
@@ -0,0 +1,12 @@
+import {
+  InternalServerErrorException,
+  createParamDecorator,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator((data, ctx) => {
+  const request = ctx.switchToHttp().getRequest();
+  const user = request.user;
+
+  if (!user) throw new InternalServerErrorException('User not found (request)');
+  return user;
+});
