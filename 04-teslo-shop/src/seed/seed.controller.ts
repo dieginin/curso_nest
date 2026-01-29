a04e0f0c99cac9a5e33d20baa7cf03150a13df78
dieginin <diegobalestra9@gmail.com>
2026-01-28 22:42:32 -0800

    Remove Auth decorator from SeedController for admin role access

--- 04-teslo-shop/src/seed/seed.controller.ts
+++ 04-teslo-shop/src/seed/seed.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { Auth } from '../auth/decorators';
 import { SeedService } from './seed.service';
-import { ValidRoles } from '../auth/interfaces';
+
+// import { Auth } from '../auth/decorators';
+// import { ValidRoles } from '../auth/interfaces';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  @Auth(ValidRoles.admin)
+  // @Auth(ValidRoles.admin)
   executeSeed() {
     return this.seedService.runSeed();
   }
