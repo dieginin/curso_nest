41be2bc4dc656dec0d60077fe257bd9e2d30fce8
dieginin <diegobalestra9@gmail.com>
2026-01-30 22:35:22 -0800

    Add socket.io dependency and implement WebSocket gateway for message handling

--- 04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
+++ 04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,22 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+
 import { MessagesWsService } from './messages-ws.service';
-import { WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket) {
+    console.log(`Client ${client.id} connected`);
+  }
+  handleDisconnect(client: Socket) {
+    console.log(`Client ${client.id} disconnected`);
+  }
 }
