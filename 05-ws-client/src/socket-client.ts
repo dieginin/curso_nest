d9950f40a27b6227fe92bdb4202361dc6fce3d07
dieginin <diegobalestra9@gmail.com>
2026-01-30 22:57:08 -0800

    Add client to consume websocket

---
+++ 05-ws-client/src/socket-client.ts
@@ -0,0 +1,7 @@
+import { Manager } from "socket.io-client"
+
+export const connectToServer = () => {
+  const manager = new Manager("localhost:3000/socket.io/socket.io.js")
+
+  const socket = manager.socket("/")
+}
